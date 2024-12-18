e57a8c0a4103d1e63d2e6fbca0a20e8a591ecf65
Rahul Qurbanov <rahulqurbanov055@gmail.com>
2024-12-18 22:40:17 +0400

    Final Part

--- src/components/Bucket.jsx
+++ src/components/Bucket.jsx
@@ -4,32 +4,32 @@ import "./Bucket.css";
 
 export default function BucketPage({ bucketName, favourites }) {
   return (
-   <div className="container-bucket">
-     <div className="bucket-page">
-      <h1 className="inp-text">{bucketName || "Your Bucket"}</h1>
+    <div className="container-bucket">
+      <div className="bucket-page">
+        <h1 className="inp-text">{bucketName || "Your Bucket"}</h1>
         {favourites.map((movie) => (
-           <div className="bucket-box">
-             <div className="bucket-box-left">
-             <img src={movie.Poster} alt="picture" />
-             <p key={movie.imdbID}>
-            <span>{movie.Title}</span>
-            <a
-              href={`https://www.imdb.com/title/${movie.imdbID}`}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-            </a>
-          </p>
-             </div>
-              <div className="bucket-box-right">
-              <button>IMDB</button>
-              </div>
-           </div>
-        ))}     
-    </div>
-         <Link to="/">
+          <div className="bucket-box" key={movie.imdbID}>
+            <div className="bucket-box-left">
+              <img src={movie.Poster} alt="picture" />
+              <p>
+                <span>{movie.Title}</span>
+              </p>
+            </div>
+            <div className="bucket-box-right">
+              <a
+                href={`https://www.imdb.com/title/${movie.imdbID}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <button>IMDB</button>
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link to="/">
         <button className="bucket-btn">Back to Home</button>
       </Link>
-   </div>
+    </div>
   );
 }
